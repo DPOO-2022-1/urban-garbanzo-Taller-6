6da7b5c09504fad493d640fbdeb5b3ccf166eed3
d.burgos <d.burgos@uniandes.edu.co>
2022-05-22 22:40:28 -0500

    carpeta normal

---
+++ Taller6_Libreria/Taller6_Libreria/src/uniandes/dpoo/taller1/modelo/YaExisteNombreEXP.java
@@ -0,0 +1,11 @@
+package uniandes.dpoo.taller1.modelo;
+
+
+public class YaExisteNombreEXP extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public YaExisteNombreEXP(String EXPerror) {
+		super(EXPerror);
+	}
+}
