3e92b211abc79efe4d9a8ac56beddb5b861178d8
dburgos26 <88894919+dburgos26@users.noreply.github.com>
2022-05-22 22:34:34 -0500

    Add files via upload

---
+++ Taller6_Libreria/src/uniandes/dpoo/taller1/modelo/CategoriaNoExisteEXP.java
@@ -0,0 +1,10 @@
+package uniandes.dpoo.taller1.modelo;
+
+public class CategoriaNoExisteEXP extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public CategoriaNoExisteEXP(String EXPerror) {
+		super(EXPerror);
+	}
+}
